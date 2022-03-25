6751e171c82b8b35ff7a65987e72cd99873f233e
Oliver Cooper <oliver.cooper@me.com>
2022-03-25 15:58:57 +1300

    Typo

--- src/listener.rs
+++ src/listener.rs
@@ -32,12 +32,14 @@ impl Listener {
     // start scanning for devices
     central.start_scan().unwrap();
 
-    central.on_event(Box::new(move |event| match event {
+    central.on_event(Box::new(move |event| {
       log::debug!("BLE event: {:?}", &event);
-      CentralEvent::DeviceDiscovered(address) | CentralEvent::DeviceUpdated(address) => {
-        tx.send(BDAddr::from(address.address)).unwrap();
+      match event {
+        CentralEvent::DeviceDiscovered(address) | CentralEvent::DeviceUpdated(address) => {
+          tx.send(BDAddr::from(address.address)).unwrap();
+        }
+        _ => (),
       }
-      _ => (),
     }));
 
     rx
