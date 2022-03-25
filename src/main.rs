69fe4d533325023473347083b94899a4579bf228
Oliver Cooper <oliver.cooper@me.com>
2022-03-25 14:01:18 +1300

    bump

--- src/main.rs
+++ src/main.rs
@@ -25,59 +25,41 @@ fn main() {
 
 use std::error::Error;
 
-use btleplug::{
-  api::{bleuuid::BleUuid, Central, CentralEvent, Manager as _, ScanFilter},
-  platform::{Adapter, Manager},
-};
 use futures::stream::StreamExt;
+use rumble::{
+  api::{Central, CentralEvent, Peripheral, UUID},
+  bluez::manager::Manager,
+};
 
 async fn get_central(manager: &Manager) -> Adapter {
   let adapters = manager.adapters().await.unwrap();
   adapters.into_iter().nth(0).unwrap()
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-  let manager = Manager::new().await?;
+pub fn main() {
+  let manager = Manager::new().unwrap();
 
   // get the first bluetooth adapter
+  let adapters = manager.adapters().unwrap();
+  let mut adapter = adapters.into_iter().nth(0).unwrap();
+
+  // reset the adapter -- clears out any errant state
+  adapter = manager.down(&adapter).unwrap();
+  adapter = manager.up(&adapter).unwrap();
+
   // connect to the adapter
-  let central = get_central(&manager).await;
-
-  // Each adapter has an event stream, we fetch via events(),
-  // simplifying the type, this will return what is essentially a
-  // Future<Result<Stream<Item=CentralEvent>>>.
-  let mut events = central.events().await?;
-
+  let central = adapter.connect().unwrap();
+  central.active(false);
+  central.filter_duplicates(false);
   // start scanning for devices
-  central.start_scan(ScanFilter::default()).await?;
+  central.start_scan().unwrap();
 
-  // Print based on whatever the event receiver outputs. Note that the event
-  // receiver blocks, so in a real program, this should be run in its own
-  // thread (not task, as this library does not yet use async channels).
-  while let Some(event) = events.next().await {
-    match event {
-      CentralEvent::DeviceDiscovered(id) => {
-        println!("DeviceDiscovered: {:?}", id);
-      }
-      CentralEvent::DeviceConnected(id) => {
-        println!("DeviceConnected: {:?}", id);
-      }
-      CentralEvent::DeviceDisconnected(id) => {
-        println!("DeviceDisconnected: {:?}", id);
-      }
-      CentralEvent::ManufacturerDataAdvertisement { id, manufacturer_data } => {
-        println!("ManufacturerDataAdvertisement: {:?}, {:?}", id, manufacturer_data);
-      }
-      CentralEvent::ServiceDataAdvertisement { id, service_data } => {
-        println!("ServiceDataAdvertisement: {:?}, {:?}", id, service_data);
-      }
-      CentralEvent::ServicesAdvertisement { id, services } => {
-        let services: Vec<String> = services.into_iter().map(|s| s.to_short_string()).collect();
-        println!("ServicesAdvertisement: {:?}, {:?}", id, services);
-      }
-      _ => {}
-    }
-  }
-  Ok(())
+  central.on_event(Box::new(|event| {
+    println!("{:}", event);
+  }));
+
+
+  // instead of waiting, you can use central.on_event to be notified of
+  // new devices
+  thread::sleep(Duration::from_secs(30));
 }
